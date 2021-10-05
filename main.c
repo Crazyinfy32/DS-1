8dbd0857fc8560f699ee79683cddc6e5101f36a1
Crazyinfy32 <86730481+Crazyinfy32@users.noreply.github.com>
2021-10-05 04:16:54 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int my_strlen(const char *str)
+
+int main()
 {
-    if(str=='\0')
-        return 0;
-    int count=0;
-    while(*str!='\0')
+    int arr[3][3]={0};
+    int i=0;
+    int j=0;
+    for(i=0;i<10;i++)
     {
-        count++;
-        str++;
+        for(j=0;j<=i;j++)
+        {
+            if(j==0)
+            {
+                arr[i][j]=1;
+            }
+            if(i==j)
+            {
+                arr[i][j]=1;
+            }
+            if(i>=2&&j>=1)
+            {
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+            }
+        }
     }
-    return count;
-}
-int main() {
-    char arr[]="abcdef";
-    printf("%d",my_strlen(arr));
+    for(i=0;i<=10;i++)
+    {
+        for(j=0;j<=10;j++)
+        {
+            printf("%d",arr[i][j]);
+        }
+        printf("/n");
+    }
     return 0;
 }
